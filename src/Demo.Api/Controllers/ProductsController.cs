08a6958efb54edde86e7a773501379d6c902a627
stuartraines <stuart2424@yahoo.com>
2020-05-28 15:47:12 +1000

    Initial commit

---
+++ src/Demo.Api/Controllers/ProductsController.cs
@@ -0,0 +1,16 @@
+using Demo.Api.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController] 
+    public class ProductsController : Controller
+    {
+        [HttpGet]
+        public IActionResult List()
+        {
+            return Ok();
+        }
+    }
+}
